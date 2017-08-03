c200b66e1c98d5eeb9c3a9153a3a79b3ea871c3c
yiming-kang <yiming.kang@wustl.edu>
2017-08-03 15:30:40 -0500

    fixed plotting relative DE probability

--- scripts/model_fitting_util.py
+++ scripts/model_fitting_util.py
@@ -387,7 +387,7 @@ def plot_holdout_features(scores_test, scores_holdout, features_var, filename, m
 					prDE_ho_med_pos, prDE_ho_med_neg = [], []
 					prDE_ho_max_pos, prDE_ho_max_neg = [], []
 					prDE_ho_min_pos, prDE_ho_min_neg = [], []
-					prDE_ho_pctl_pos, prDE_ho_pctl_neg = [], []
+					prDE_ho_pctl_pos, prDE_ho_pctl_neg = [[],[]], [[],[]]
 					for l in range(prDE_ho.shape[1]):
 						prDE_ho_pos = prDE_ho_sub[:,l][prDE_ho[:,l] > .5]
 						prDE_ho_neg = prDE_ho_sub[:,l][prDE_ho[:,l] < .5]
@@ -397,10 +397,10 @@ def plot_holdout_features(scores_test, scores_holdout, features_var, filename, m
 						prDE_ho_max_neg.append(np.max(prDE_ho_neg))
 						prDE_ho_min_pos.append(np.min(prDE_ho_pos))
 						prDE_ho_min_neg.append(np.min(prDE_ho_neg))
-						prDE_ho_pctl_pos = [np.percentile(prDE_ho_pos, 2.5, axis=0), 
-											np.percentile(prDE_ho_pos, 97.5, axis=0)]
-						prDE_ho_pctl_neg = [np.percentile(prDE_ho_neg, 2.5, axis=0), 
-											np.percentile(prDE_ho_neg, 97.5, axis=0)]
+						prDE_ho_pctl_pos[0].append(np.percentile(prDE_ho_pos, 2.5))
+						prDE_ho_pctl_pos[0].append(np.percentile(prDE_ho_pos, 97.5))
+						prDE_ho_pctl_neg[0].append(np.percentile(prDE_ho_neg, 2.5))
+						prDE_ho_pctl_neg[0].append(np.percentile(prDE_ho_neg, 97.5))
 					## make plots
 					ax = fig.add_subplot(num_rows, num_cols, k+1)
 					ax.fill_between(features_var[features[k]], prDE_ho_pctl_pos[0], prDE_ho_pctl_pos[1], facecolor="#0066cc", alpha=.25)
